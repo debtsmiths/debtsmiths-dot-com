6ce2cd74d569f722836faf2c13203a9bc0e34ec1
Ephraim Smith <ephraimjsmith@gmail.com>
2021-01-08 09:23:04 -0600

    generic LandingPage to test configs

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/debtsmiths-logo.png';
+
+const LandingPage = () => {
+  return (
+    <main className="LandingPage">
+      <img src={logo} alt="Debtsmiths logo" height="auto" width="auto"/>
+    </main>
+  )
+}
+
+export default LandingPage
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
